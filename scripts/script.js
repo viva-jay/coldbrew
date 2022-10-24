12881104b51c808f97fd2e5b52d9c4b22c05f8fa
hoon0912 <hoon0912@gmail.com>
2022-10-24 14:20:29 +0900

    add k6

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+import { check } from "k6";
+
+export let options = {
+    duration: '1s',
+    vus: 1,
+
+    thresholds: {
+        "http_req_duration": [`p(95) < 500`],
+    },
+};
+
+export default function () {
+    let response = http.get("http://www.google.com");
+    let length = response.body.length
+    check(length, { "body length ": (v) => v >= 0});
+};
