a8e2c0787ad2d05d81a2554cdffe44c8b6432e13
viva-jay <hoon0912@gmail.com>
2022-10-26 11:28:53 +0900

    add k6

---
+++ k6/scripts/script.js
@@ -0,0 +1,23 @@
+import http from 'k6/http';
+import { check } from "k6";
+
+export let options = {
+    ext: {
+        loadimpact: {
+            projectID: 3606293,
+            name: "streaming to cloud"
+        }
+    },
+    duration: '1s',
+    vus: 1,
+
+    thresholds: {
+        "http_req_duration": [`p(95) < ${__ENV.REQ_DURATION_THRESHOLDS}`],
+    },
+};
+
+export default function () {
+    let response = http.get("http://www.google.com");
+    let length = response.body.length
+    check(length, { "body length ": (v) => v >= 0});
+};
